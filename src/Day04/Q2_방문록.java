d47d31efe99daf6630a69aad0f490083014e69c4
501t <itdanja@kakao.com>
2022-12-15 21:48:45 +0900

    day04 Q2 방문록 프로그램

---
+++ src/Day04/Q2_방문록.java
@@ -0,0 +1,25 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class Q2_방문록 {
+    public static void main(String[] args) {
+
+        // 힌트
+        Scanner scanner = new Scanner(System.in);
+        String output = "";
+        System.out.println("입력 : "); output += scanner.next();
+        System.out.println("출력 : " + output );
+        System.out.println("입력2 : "); output += scanner.next();
+        System.out.println("출력2 : " + output );
+
+    }
+}
+/*
+    조건1 : 제어문,배열,반복문 사용금지
+    조건2 : 방문록 5번 작성한다.
+    결과물 : 아래 그림 첨부
+    정답/질문 알고싶으면 : itdanja@kakao.com
+
+ */
+
