0dc14388bbe270cabfe2c5e5cd3cc473a42e6bc0
501t <itdanja@kakao.com>
2022-12-22 21:09:03 +0900

    day05 IF 문제

--- src/Day05/Q1_IF문제.java
+++ src/Day05/Q1_IF문제.java
@@ -1,13 +1,35 @@
 package Day05;
 
-public class Q1_IF문제 {
-
-    public static void main(String[] args) {
-
+import java.util.Scanner;
+// java.util 패키지에서 Scanner 클래스 가져오기
 
 
-    }
-}
+public class Q1_IF문제 { // class s
+    public static void main(String[] args) { // m s
+
+        //  * 입력객체 만들기
+            // Scanner 클래스 사용시 클래스 위에 import 하자
+        Scanner scanner = new Scanner(System.in);
+        /* ------------------------ 1 ----------------------------*/
+            // 1. [출력] 입력 전 입력받을 내용을 안내해주기
+        System.out.print("문제1 : ");
+            // 2. [입력/저장] scanner 객체로 int형으로 입력받은 값 가져와서 문제1 변수에 저장
+        int 문제1 = scanner.nextInt();
+            // 3. [제어] 만약에 입력값이 나누기 2 했을때 나머지가 0 이면 짝수 아니면 홀수
+        if( 문제1 % 2 == 0 ) System.out.println("짝수입니다.");
+        else System.out.println("홀수입니다.");
+        /* ---------------------- 2 ------------------------------*/
+
+        // * 현재 같은 { } 안에 scanner 객체가 있으면 또 작성할 필요가 없다.
+        System.out.print("문제2 : ");
+        int 문제2 = scanner.nextInt();
+        if( 문제2 % 7 == 0 ) System.out.println("7배수입니다.");
+        else System.out.println("7배수가 아닙니다.");
+
+        /* ---------------------- 3 ------------------------------*/
+
+    } // m s
+} // class e
 /*
     [ 삼항연산자 X ]
     문제1 : 입력받은 값이 홀수 이면 '홀수입니다' 아니면 '짝수입니다'
@@ -21,17 +43,13 @@ public class Q1_IF문제 {
         아니면  '불합격'
     문제4 : 2개의 정수를 입력받아 가장 큰수 출력
         입력 예) 3 5       출력 예 ) 3 이 가장 큽니다.
-
     문제5 : 3개의 정수를 입력받아 가장 큰수 출력
         입력 예) 3 5 2      출력 예 ) 5 이 가장 큽니다.
-
     문제6 : 4개의 정수를 입력받아 가장 큰수 출력
         입력 예) 3 5 2 8     출력 예 ) 8 이 가장 큽니다.
-
-    문제7 : 3개의 정수를 입력받아 가장 오름차순으로 출력
+    문제7 : 3개의 정수를 입력받아 오름차순으로 출력
         입력 예) 3 5 2      출력 예 ) 2 3 5
-
-    문제8 : 4개의 정수를 입력받아 가장 내림차순 출력
+    문제8 : 4개의 정수를 입력받아 내림차순 출력
         입력 예) 3 5 2 8     출력 예 ) 8 5 3 2
 
  */
